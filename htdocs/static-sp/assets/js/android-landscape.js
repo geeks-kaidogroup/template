fcefba213b22205e3054f785c4fc4a1d579a134e
Takashi Uehara <uehara@kaido-group.co.jp>
2015-06-08 16:26:18 +0900

    2015年6月1日施行版

---
+++ htdocs/static-sp/assets/js/android-landscape.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+var portraitWidth,landscapeWidth;
+$(window).bind("resize", function(){
+	if(Math.abs(window.orientation) === 0){
+		if(/Android/.test(window.navigator.userAgent)){
+			if(!portraitWidth)portraitWidth=$(window).width();
+		}else{
+			portraitWidth=$(window).width();
+		}
+		$("html").css("zoom" , portraitWidth/640 );
+	}else{
+		if(/Android/.test(window.navigator.userAgent)){
+			if(!landscapeWidth)landscapeWidth=$(window).width();
+		}else{
+			landscapeWidth=$(window).width();
+		}
+		$("html").css("zoom" , landscapeWidth/640 );
+	}
+}).trigger("resize");
